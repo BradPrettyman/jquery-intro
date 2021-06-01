e7702cc9d4bfab3a1a6c254263b15d867d66accb
Brad Prettyman <brad.prettyman5@gmail.com>
2021-06-01 13:35:22 -0700

    add jquery effects for walrus facts

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 jQuery(document).ready(function() {
-  jQuery("h1").click(function() {
-    alert("This is a heading.");
-    alert("I told you, THIS IS A HEADING!");
-  });
+  // jQuery("h1").click(function() {
+  //   alert("This is a heading.");
+  //   alert("I told you, THIS IS A HEADING!");
+  // });
 
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
+  // jQuery("p").click(function() {
+  //   alert("This is a paragraph.");
+  // });
 
-  jQuery("img").click(function() {
-    alert("This is an image.");
-  });
+  // jQuery("img").click(function() {
+  //   alert("This is an image.");
+  // });
 });
 
 $(document).ready(function() {
@@ -18,4 +18,13 @@ $(document).ready(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+
+  $('.walrus-facts button').on('click', function () {
+    $('.effect-fadein').fadeIn();
+    $('.effect-fadeout').fadeOut();
+    $('.effect-fadetoggle').fadeToggle();
+    $('.effect-slideup').slideUp();
+    $('.effect-slidedown').slideDown();
+    $('.effect-slidetoggle').slideToggle();
+  })
 });

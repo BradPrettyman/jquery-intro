93dbceb44d92960de6b81811d4216f89cbb08787
Brad Prettyman <brad.prettyman5@gmail.com>
2021-06-01 12:15:38 -0700

    add jquery styling and more css

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-jQuery("h1").click(function() {
-  alert("This is a heading.");
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("This is a heading.");
+    alert("I told you, THIS IS A HEADING!");
+  });
+
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
 });
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
-
-jQuery("img").click(function() {
-  alert("This is an image.");
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
 });
